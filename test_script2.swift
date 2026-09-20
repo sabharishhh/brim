feb755d852cca89d1bbe8b8891999b8c5ba359f3
Brim <brim@google.com>
2026-09-20 17:54:33 +0530

    Clean up scratch test scripts

--- test_script2.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-let path = FileManager.default.temporaryDirectory.path
-var buffer = [CChar](repeating: 0, count: Int(PATH_MAX))
-if realpath(path, &buffer) != nil {
-    print(String(cString: buffer))
-} else {
-    print("realpath failed")
-}
